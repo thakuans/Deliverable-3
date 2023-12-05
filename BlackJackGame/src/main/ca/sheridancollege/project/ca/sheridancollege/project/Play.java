04c4241e2742b00daf0f30ee8796ac2d0bb6ef4b
thakuans <thakuans@sheridancollege.ca>
2023-12-05 08:18:17 +0530

    Update Play.java
    Added functionality to give more rounds to user and a option to quit the game

--- BlackJackGame/src/main/ca/sheridancollege/project/ca/sheridancollege/project/Play.java
+++ BlackJackGame/src/main/ca/sheridancollege/project/ca/sheridancollege/project/Play.java
@@ -1,4 +1,7 @@
 package ca.sheridancollege.project;
+
+import java.util.Scanner;
+
 /**
  * SYST 17796 Black Jack Card Game
  * @author Anshika 
@@ -10,7 +13,41 @@ public class Play {
  * Run this program and start playing
  */
     public static void main(String[] args) {
-        Game game = new Game();
-        game.play();
+        Scanner scanner = new Scanner(System.in);
+
+        int totalRounds = 0;
+        int roundsWon = 0;
+        int roundsLost = 0;
+
+        // Run the game until the user opts to quit
+        while (true) {
+            totalRounds++;
+            
+            Game game = new Game();
+            game.play();
+
+            
+
+            // Display a summary of the play
+            displaySummary(totalRounds, roundsWon, roundsLost);
+
+            // Ask the user if they want to play again
+            System.out.print("Do you want to play again? (y/n): ");
+            String playAgain = scanner.next();
+
+            if (!playAgain.equalsIgnoreCase("y")) {
+                System.out.println("User Quitted the Game");
+                // System.out.println("You played "+totalRounds+"rounds");
+                System.out.println("Thanks for playing! Goodbye.");
+                break; // Exit the loop if the user chooses not to play again
+            }
+        }
+
+        scanner.close();
+    }
+
+    private static void displaySummary(int totalRounds, int roundsWon, int roundsLost) {
+        System.out.println("Total Rounds Played: " + totalRounds);
+        
     }
 }
